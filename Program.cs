48613d5d8ceac67ac289f921c9d9344a70eb4598
Juan <casal.juanmanuel@gmail.com>
2020-10-17 21:12:57 -0300

    creando proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_Clase_2_Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
